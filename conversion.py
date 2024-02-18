ab2225021edd299c4e39cf9eca04c1a9ec451a93
annabelgibson1997 <153768779+annabelgibson1997@users.noreply.github.com>
2024-02-18 17:58:08 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,35 @@
+#Declare the following variables and their values:
+# num1 = 99.23
+# num2 = 23
+# num3 = 150
+# string1 = "100"
+#First, convert num1 into an integer (use int())
+#Second, convert num2 into a float (use float())
+#Third, convert num3 into a string (use str())
+#Fourth, convert string1 into an integer (use int())
+#Finally, print all of the variables out on separate lines.
+# Double check you have converted each variable currectly
+# using the command: print(type(name of variable)) 
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+print(num1)
+print(num2)
+print(num3)
+print(string1)
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(string1))
+#I understand this final part wasn't in the task.
+#However, I wanted to understand if I had done the task correctly.
+#Hope this is okay.
